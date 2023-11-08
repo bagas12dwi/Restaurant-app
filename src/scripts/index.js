cb92b9ad9f48af20ea450770347fad74015bb925
bagas12dwi <bag12dwi.s@gmail.com>
2023-11-08 17:14:02 +0700

    initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,22 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/responsive.css';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+import App from './views/app';
+import swRegister from './utils/sw-register';
+
+const app = new App({
+  button: document.querySelector('#menu'),
+  drawer: document.querySelector('#navmenu'),
+  content: document.querySelector('#mainContent'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+  swRegister();
+});
